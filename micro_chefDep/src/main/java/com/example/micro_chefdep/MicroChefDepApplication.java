c97c6230ef2b2a273e0cfd3c2aceae04db228e92
hamza <hamzabaroudu2000@gmail.com>
2022-04-01 20:04:12 +0000

    init webservice CheDep

---
+++ micro_chefDep/src/main/java/com/example/micro_chefdep/MicroChefDepApplication.java
@@ -0,0 +1,13 @@
+package com.example.micro_chefdep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroChefDepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroChefDepApplication.class, args);
+    }
+
+}
