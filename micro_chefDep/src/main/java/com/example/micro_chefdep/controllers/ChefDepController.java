2089e268b33a75eea0afebe912a9127c521b5a28
hamza <hamzabaroudu2000@gmail.com>
2022-04-05 20:17:36 +0000

    Add service package ChefDep

--- micro_chefDep/src/main/java/com/example/micro_chefdep/controllers/ChefDepController.java
+++ micro_chefDep/src/main/java/com/example/micro_chefdep/controllers/ChefDepController.java
@@ -2,9 +2,6 @@ package com.example.micro_chefdep.controllers;
 
 import com.example.micro_chefdep.Services.ChefDepService;
 import com.example.micro_chefdep.models.ChefDep;
-import com.example.micro_chefdep.repositories.ChefDefRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 
