0df5347516b35390167d7b1c79b5da0c520ba3bf
Admin <Admin@PC-ADMIN>
2022-04-06 23:51:05 +0100

    hello

--- micro_authentification/src/main/java/com/gestion_ressource/micro_authentification/MicroAuthentificationApplication.java
+++ micro_authentification/src/main/java/com/gestion_ressource/micro_authentification/MicroAuthentificationApplication.java
@@ -1,5 +1,10 @@
 package com.gestion_ressource.micro_authentification;
 
+import com.gestion_ressource.micro_authentification.model.ERole;
+import com.gestion_ressource.micro_authentification.model.Role;
+import com.gestion_ressource.micro_authentification.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,10 +12,19 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class MicroAuthentificationApplication {
+public class MicroAuthentificationApplication implements CommandLineRunner{
 
+    @Autowired
+    RoleRepository repository;
     public static void main(String[] args) {
         SpringApplication.run(MicroAuthentificationApplication.class, args);
     }
-
+    @Override
+    public void run(String... args) throws Exception {
+        repository.save(new Role(ERole.ROLE_ADMIN));
+        repository.save(new Role(ERole.ROLE_CHEF_DEP));
+        repository.save(new Role(ERole.ROLE_PROF));
+        repository.save(new Role(ERole.ROLE_CHEF_RESOURCES));
+        repository.save(new Role(ERole.ROLE_PROVIDER));
+    }
 }
