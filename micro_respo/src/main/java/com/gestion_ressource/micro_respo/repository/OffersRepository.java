1b0b3b6b40ed6e3cddff42091fd50abf598f1531
Admin <Admin@PC-ADMIN>
2022-05-22 16:12:09 +0100

    offer

--- micro_respo/src/main/java/com/gestion_ressource/micro_respo/repository/OffersRepository.java
+++ micro_respo/src/main/java/com/gestion_ressource/micro_respo/repository/OffersRepository.java
@@ -2,6 +2,9 @@ package com.gestion_ressource.micro_respo.repository;
 
 import com.gestion_ressource.micro_respo.model.Offer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface OffersRepository extends MongoRepository<Offer, String> {
 }
